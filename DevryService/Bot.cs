16c6c895769cbf744a0cc6a3fc2c5c5dfd2d3d23
Jon Braunsma <jonbraunsma@gmail.com>
2021-01-13 18:34:11 -0500

    test

--- DevryService/Bot.cs
+++ DevryService/Bot.cs
@@ -47,11 +47,13 @@ namespace DevryService
             var bytes = Convert.FromBase64String(System.IO.File.ReadAllText("token.cfg"));
             token = System.Text.Encoding.UTF8.GetString(bytes);
             Console.WriteLine("Acquired PROD Token");
+            Console.WriteLine(System.IO.File.ReadAllText("token.cfg").Length);
 #else
+
             token = config.GetValue<string>("token");
             Console.WriteLine("Acquired DEBUG Token");
 #endif
-
+            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
             Discord = new DiscordClient(new DiscordConfiguration
             {
                 Token = token,
